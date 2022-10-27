463dae9e2502d8752934379a9c81b7f4211ea6b5
xgroleau 🐢 <xavgroleau@gmail.com>
2022-10-27 15:59:00 -0400

    Added a safer API

--- examples/usage.rs
+++ examples/usage.rs
@@ -1,13 +1,22 @@
 #![no_std]
 #![no_main]
 
-use defmt_rtt as _; // global logger
+use defmt_rtt as _;
 use panic_probe as _;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    let buff = persistant_buff::PersistantBuff::take().unwrap();
-    buff[0] += 1;
+    let mut pbuff = persistant_buff::PersistantBuff::take_managed().unwrap();
+
+    // Verify the address
+    let addr = unsafe { persistant_buff::PersistantBuff::steal() as *const [u8] as *const u8 };
+    defmt::info!("Address {}", addr);
+
+    // Trivial way to initialize it to fill it with 0
+    let buff = pbuff.validate(|b| b.fill(0));
+
+    buff[0] = (buff[0] % 255) + 1;
     defmt::info!("Value is now {}", buff[0]);
+    cortex_m::asm::bkpt();
     loop {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -18,21 +18,20 @@
 //! ``` ignore
 //! MEMORY
 //! {
-//!   /* NOTE K = KiBi = 1024 bytes */
-//!   FLASH : ORIGIN  = 0x00000000, LENGTH = 512K
-//!   RAM : ORIGIN    = 0x20000000, LENGTH = 64K
+//!   /* NOTE 1 K = 1 KiBi = 1024 bytes */
+//!   FLASH : ORIGIN = 0x00000000, LENGTH = 1024K
+//!   RAM : ORIGIN = 0x20000000, LENGTH = 128K
 //! }
 //! ```
 //! `memory.x` file after modification to hold a 1K region:
 //! ``` ignore
 //! MEMORY
 //! {
-//!   /* NOTE K = KiBi = 1024 bytes */
-//!   FLASH : ORIGIN  = 0x00000000, LENGTH = 512K
-//!   RAM : ORIGIN    = 0x20000000, LENGTH = 63K
-//!   PERSISTANT_BUFF: ORIGIN = 0x2000FC00, LENGTH = 1K
+//!   /* NOTE 1 K = 1 KiBi = 1024 bytes */
+//!   FLASH : ORIGIN = 0x00000000, LENGTH = 1024K
+//!   RAM : ORIGIN = 0x20000000, LENGTH = 128K - 1K
+//!   PERSISTANT_BUFF: ORIGIN = ORIGIN(RAM) + LENGTH(RAM), LENGTH = 1K
 //! }
-//!
 //! _persistant_buff_start = ORIGIN(PERSISTANT_BUFF);
 //! _persistant_buff_end   = ORIGIN(PERSISTANT_BUFF) + LENGTH(PERSISTANT_BUFF);
 //! ```
@@ -45,8 +44,9 @@
 //! #[entry]
 //! fn main() -> ! {
 //!     let buff = persistant_buff::PersistantBuff::take().unwrap();
-//!     buff[0]++;
-//!     info!("Value is now {}", buff[0]);
+//!     buff[0] += 1;
+//!     info!("Value is now {}", buff[0]); // Every reboot the value will change
+//!     ///...
 //! }
 //! ```
 #![no_std]
@@ -55,12 +55,38 @@
 
 use core::sync::atomic::{AtomicBool, Ordering};
 
+const MAGIC_NUMBER: u32 = 0x42069F;
 static mut PERSISTANT_BUFF_TAKEN: AtomicBool = AtomicBool::new(false);
 
-/// Strut to request the persistant buff
-pub struct PersistantBuff();
+/// Strut to request the persistant buff and manage it `safely`.
+/// When acquiring the buffer you need to validate it and init it to a known sate
+pub struct PersistantBuff {
+    magic: *mut u32,
+    buff: &'static mut [u8],
+}
+
 impl PersistantBuff {
-    /// Get the persistant buff
+    /// Take a managed version fo the persistant buff.
+    /// Allow to check if the buffer is valid or not before usage
+    /// Note that vs the [Self::take] function, you will lose sii
+    pub fn take_managed() -> Option<Self> {
+        Self::take().map(|b| Self {
+            magic: b.as_mut_ptr().cast::<u32>(),
+            buff: &mut b[core::mem::size_of::<u32>()..],
+        })
+    }
+
+    /// Steal a managed version for the persistant buff without check
+    /// See [Self::take_managed]
+    pub unsafe fn steal_managed() -> Self {
+        let b = Self::steal();
+        Self {
+            magic: b.as_mut_ptr().cast::<u32>(),
+            buff: &mut b[core::mem::size_of::<u32>()..],
+        }
+    }
+
+    /// Get the raw persistant buff
     pub fn take() -> Option<&'static mut [u8]> {
         unsafe {
             if PERSISTANT_BUFF_TAKEN.swap(true, Ordering::Relaxed) {
@@ -72,7 +98,7 @@ impl PersistantBuff {
     }
 
     /// Steal the persistant buff.
-    /// Ignore if it was already taken .
+    /// Ignore if it was already taken.
     pub unsafe fn steal() -> &'static mut [u8] {
         PERSISTANT_BUFF_TAKEN.store(true, Ordering::SeqCst);
         extern "C" {
@@ -85,5 +111,31 @@ impl PersistantBuff {
 
         let slice = core::slice::from_raw_parts_mut(start, len);
         slice
+    }
+
+    /// Mark the persistant buffer with valid data in it
+    fn mark(&mut self) {
+        unsafe {
+            *self.magic = MAGIC_NUMBER;
+        }
+    }
+
+    /// Verify if the persistant buffer has valid data in it
+    fn check(&self) -> bool {
+        unsafe { *self.magic == MAGIC_NUMBER }
+    }
+
+    /// Check if the buffer is valid, if not call the provided closure
+    /// Then mark the buffer as valid and initialize it to a known state.
+    /// It's to make sure the data in it is always "valid" and not garbage.
+    pub fn validate<F>(&mut self, f: F) -> &mut [u8]
+    where
+        F: FnOnce(&mut [u8]),
+    {
+        if !self.check() {
+            f(self.buff)
+        }
+        self.mark();
+        self.buff
     }
 }
